8ea4bd3012a44b34c5451392ecebf4d1e4e5f0ae
jonct <2807816+jonct@users.noreply.github.com>
2024-07-07 18:51:58 -0400

    Test jail networking quickly; bail if needed

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,14 @@ set -euo pipefail
 # TODO: create a path and/or zfs pool with a space in it to test if jlmkr.py still works properly when ran from inside
 # mkdir -p "/tmp/path with space/jailmaker"
 
+./jlmkr.py create examiner \
+    --network-veth \
+    --system-call-filter='add_key keyctl bpf' \
+    --config=./test/examiner-config \
+    --start
+
+exit
+
 # TODO: test jlmkr.py from inside another working directory, with a relative path to a config file to test if it uses the config file (and doesn't look for it relative to the jlmkr.py file itself)
 ./jlmkr.py create --start --config=./templates/docker/config test --network-veth --system-call-filter='add_key' --system-call-filter='bpf' --system-call-filter='keyctl'
 ./jlmkr.py exec test docker run hello-world
