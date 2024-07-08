91554733191e30365ce5017a0ca3e8cfe670f66f
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2024-07-08 13:58:44 +0200

    Create bridge with systemd-networkd

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 # TODO: create a path and/or zfs pool with a space in it to test if jlmkr.py still works properly when ran from inside
 # mkdir -p "/tmp/path with space/jailmaker"
 
+# TODO: test jlmkr.py from inside another working directory, with a relative path to a config file to test if it uses the config file (and doesn't look for it relative to the jlmkr.py file itself)
+./jlmkr.py create --start --config=./templates/docker/config test
+./jlmkr.py exec test docker run hello-world
+
+# TODO: many more test cases and checking if actual output (text, files on disk etc.) is correct instead of just a 0 exit code
+
 ./jlmkr.py create examiner
 ./jlmkr.py start examiner
 ./jlmkr.py exec examiner bash <<EOF
@@ -18,11 +24,3 @@ ping -c3 192.168.123.1
 ping -c3 deb.debian.org
 netstat -n -r
 EOF
-
-exit
-
-# TODO: test jlmkr.py from inside another working directory, with a relative path to a config file to test if it uses the config file (and doesn't look for it relative to the jlmkr.py file itself)
-./jlmkr.py create --start --config=./templates/docker/config test --network-veth --system-call-filter='add_key' --system-call-filter='bpf' --system-call-filter='keyctl'
-./jlmkr.py exec test docker run hello-world
-
-# TODO: many more test cases and checking if actual output (text, files on disk etc.) is correct instead of just a 0 exit code
