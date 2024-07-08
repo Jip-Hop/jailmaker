fd9e8bf08b32f09e5cf978117bbb16cae8d347eb
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2024-07-08 16:06:13 +0200

    Debug in jail

--- test/test.sh
+++ test/test.sh
@@ -4,23 +4,24 @@ set -euo pipefail
 # TODO: create a path and/or zfs pool with a space in it to test if jlmkr.py still works properly when ran from inside
 # mkdir -p "/tmp/path with space/jailmaker"
 
-# TODO: test jlmkr.py from inside another working directory, with a relative path to a config file to test if it uses the config file (and doesn't look for it relative to the jlmkr.py file itself)
-./jlmkr.py create --start --config=./templates/docker/config test
-./jlmkr.py exec test docker run hello-world
-
 # TODO: many more test cases and checking if actual output (text, files on disk etc.) is correct instead of just a 0 exit code
 
-./jlmkr.py create examiner
-./jlmkr.py start examiner
+./jlmkr.py create --start examiner
+echo "About to run debug logging in jail"
 ./jlmkr.py exec examiner bash <<EOF
 for path in /etc/systemd/network* /etc/systemd/resolve* /etc/resolv.conf ; do
 	echo "✳️ \$path"
 	[ -d "\$path" ] && ls -la "\$path" || cat "\$path"
 	echo
 done
+netstat -n -r
+sleep 3
 ip addr
 resolvectl query deb.debian.org
-ping -c3 192.168.123.1
-ping -c3 deb.debian.org
-netstat -n -r
+ping -c1 1.1.1.1
+ping -c1 deb.debian.org
 EOF
+
+# TODO: test jlmkr.py from inside another working directory, with a relative path to a config file to test if it uses the config file (and doesn't look for it relative to the jlmkr.py file itself)
+./jlmkr.py create --start --config=./templates/docker/config test
+./jlmkr.py exec test docker run hello-world
