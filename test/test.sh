3eaf9cf8c3301d1ec1d7466c635019e42fc55a03
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2024-07-08 16:13:07 +0200

    Create and run debug script

--- test/test.sh
+++ test/test.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 
 ./jlmkr.py create --start examiner
 echo "About to run debug logging in jail"
-./jlmkr.py exec examiner bash <<EOF
+cat <<EOF > jails/examiner/rootfs/root/debug.sh
 for path in /etc/systemd/network* /etc/systemd/resolve* /etc/resolv.conf ; do
 	echo "✳️ \$path"
 	[ -d "\$path" ] && ls -la "\$path" || cat "\$path"
@@ -21,6 +21,8 @@ resolvectl query deb.debian.org
 ping -c1 1.1.1.1
 ping -c1 deb.debian.org
 EOF
+
+./jlmkr.py exec examiner bash /root/debug.sh
 
 # TODO: test jlmkr.py from inside another working directory, with a relative path to a config file to test if it uses the config file (and doesn't look for it relative to the jlmkr.py file itself)
 ./jlmkr.py create --start --config=./templates/docker/config test
