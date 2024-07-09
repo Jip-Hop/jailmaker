be51af67dc4906ad5c8f26cce2915264d09a634a
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2024-07-09 05:05:53 +0200

    Test with docker again

--- test/test.sh
+++ test/test.sh
@@ -8,5 +8,5 @@ set -euo pipefail
 
 # TODO: test jlmkr.py from inside another working directory, with a relative path to a config file to test if it uses the config file (and doesn't look for it relative to the jlmkr.py file itself)
 
-./jlmkr.py create --start --config=./templates/podman/config test
-./jlmkr.py exec test podman run hello-world
+./jlmkr.py create --start --config=./templates/docker/config test
+./jlmkr.py exec test docker run hello-world
