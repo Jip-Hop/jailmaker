9690a5a430b29f340312d0a51f69dd1b64d46eca
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2024-07-09 13:43:19 +0200

    Remove debug logging

--- test/test.sh
+++ test/test.sh
@@ -8,11 +8,5 @@ set -euo pipefail
 
 # TODO: test jlmkr.py from inside another working directory, with a relative path to a config file to test if it uses the config file (and doesn't look for it relative to the jlmkr.py file itself)
 
-./jlmkr.py create --start --config=./templates/docker/config test || true
-./jlmkr.py exec test systemctl status docker || true
-./jlmkr.py exec test journalctl -xeu docker.service || true
-./jlmkr.py exec test ip a || true
-
-echo "Ubuntu host:"
-ip a
-# ./jlmkr.py exec test docker run hello-world
+./jlmkr.py create --start --config=./templates/docker/config test
+./jlmkr.py exec test docker run hello-world
