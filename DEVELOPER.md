acea4345f83a0f3b19d811f237fc40950ca5ff13
jonct <2807816+jonct@users.noreply.github.com>
2024-08-08 03:50:35 -0400

    Take another run at edit/build/testing in place

---
+++ DEVELOPER.md
@@ -0,0 +1,46 @@
+# Developer notes
+
+## Building Jailmaker
+
+No external dependencies are needed to perform a simple build from the project directory.
+
+    python3 build.py
+
+Anything beyond this is *completely optional…*
+
+## Development mode
+
+Jailmaker's user-facing design and important safety features tend to get in the way of rapid development. To run directly from the editable source code, create an external configuration file.
+
+    mkdir -p ~/.local/share
+    cat <<EOF >~/.local/share/jailmaker.conf
+    [DEFAULT]
+    ignore_owner = 1
+    jailmaker_dir = /mnt/pool/jailmaker
+    EOF
+
+If present, this file will override traditional self-detection of the Jailmaker directory.
+
+## Code quality tooling
+
+Additional tools for testing, coverage, and code quality review are available through [Hatch][1]. Install them in a self-contained, disposable virtual environment.
+
+    python3 -m venv --without-pip .venv
+    curl -OL https://bootstrap.pypa.io/pip/pip.pyz
+    .venv/bin/python3 pip.pyz install pip hatch
+    rm pip.pyz
+
+Activate a session inside the virtual environment. (For more information see the `venv` [tutorial][2].)
+
+    source .venv/bin/activate
+
+Use `hatch` to build, test, lint, etc.
+
+    hatch build
+
+## Integration testing
+
+See [`test/README.md`](./test/README.md).
+
+[1]: https://hatch.pypa.io/
+[2]: https://docs.python.org/3/tutorial/venv.html
--- src/jlmkr/paths.py
+++ src/jlmkr/paths.py
@@ -2,20 +2,66 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-only
 
-import os.path
+import os
+import sys
 
-# When running as a zipapp, the script file is a parent
-ZIPAPP_PATH = os.path.realpath(__file__)
-while not os.path.exists(ZIPAPP_PATH):
-    ZIPAPP_PATH = os.path.dirname(ZIPAPP_PATH)
+from configparser import ConfigParser
+from pathlib import Path
+from utils.console import fail
 
-SCRIPT_PATH = os.path.realpath(ZIPAPP_PATH)
-SCRIPT_NAME = os.path.basename(SCRIPT_PATH)
-SCRIPT_DIR_PATH = os.path.dirname(SCRIPT_PATH)
-COMMAND_NAME = os.path.basename(ZIPAPP_PATH)
 
-JAILS_DIR_PATH = os.path.join(SCRIPT_DIR_PATH, "jails")
+def _get_selected_jailmaker_directory() -> Path:
+    '''
+    Determine the user's affirmative choice of parent jailmaker directory
+    '''
+    # first choice: global --dir/-D argument
+    #TODO
+    
+    # next: JAILMAKER_DIR environment variable
+    envname = 'JAILMAKER_DIR'
+    if envname in os.environ:
+        return Path(os.environ[envname])
+    
+    # next: ~/.local/share/jailmaker.conf
+    secname = 'DEFAULT'
+    cfgname = 'jailmaker_dir'
+    username = ''
+    if os.getuid() == 0 and 'SUDO_USER' in os.environ:
+        username = os.environ['SUDO_USER']
+    cfgpath = Path(f'~{username}/.local/share/jailmaker.conf').expanduser()
+    cfg = ConfigParser()
+    cfg.read(cfgpath)
+    if 'ignore_owner' in cfg[secname]:
+        os.environ['JLMKR_DEBUG'] = cfg[secname]['ignore_owner']
+    if cfgname in cfg[secname]:
+        return Path(cfg[secname][cfgname])
+    
+    # next: current directory iff it's named jailmaker
+    script = Path(sys.argv[0]).resolve(True)
+    if script.parent.name == 'jailmaker':
+        return script.parent
+    
+    fail("Please specify a jailmaker directory path (JAILMAKER_DIR)")
+
+
+def get_tool_path_on_disk() -> Path:
+    '''
+    Determine the script's location on disk
+    '''
+    # When running as a zipapp, the script file is an ancestor
+    path = Path(__file__).resolve(strict=False)
+    while path and not path.is_file():
+        path = path.parent
+    return path
+
+
+SCRIPT_PATH = get_tool_path_on_disk()
+SCRIPT_NAME = SCRIPT_PATH.name
+COMMAND_NAME = SCRIPT_NAME
+SHORTNAME = "jlmkr"
+
+JAILMAKER_DIR_PATH = _get_selected_jailmaker_directory()
+
+JAILS_DIR_PATH = JAILMAKER_DIR_PATH.joinpath("jails")
 JAIL_CONFIG_NAME = "config"
 JAIL_ROOTFS_NAME = "rootfs"
-
-SHORTNAME = "jlmkr"
