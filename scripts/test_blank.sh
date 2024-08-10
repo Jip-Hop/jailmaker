5c8b41061f6d8c39ed7141b16815645bff3f284d
jonct <2807816+jonct@users.noreply.github.com>
2024-08-10 01:43:46 -0400

    Document lifted restriction on test stragglers

--- scripts/test_blank.sh
+++ scripts/test_blank.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 # $JAILMAKER_DIR -> Jailmaker directory (also current working dir)
 # $TEMPLATES -> path to project templates directory
 
-# Side-effects are a problem. We can't sandbox network settings,
-# etc. and we can only clean up ZFS if no jails are left running.
+# Side-effects are a problem. We can't sandbox network settings, etc.
 
 
 sudo -E ./jlmkr create blankjail
